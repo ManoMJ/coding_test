79308a64b5b9e132f8490aafbdad7be962656c79
ManoMJ <46708194+ManoMJ@users.noreply.github.com>
2024-07-26 10:17:52 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 6122.93 ms, Memory: 43.5 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,15 +1,11 @@
 def solution(elements):
-    n = len(elements)
-    circular_elements = elements * 2  # 원형 수열 만들기 위해 두 번 이어붙임
-    sums = set()  # 부분 수열의 합을 저장할 집합
-
-    # 모든 길이의 부분 수열을 구함
-    for length in range(1, n + 1):
-        for start in range(n):
-            sub_sum = sum(circular_elements[start:start + length])
+    sums = set()
+    c_elements = elements*2
+    for i in range(1, len(elements)+1):
+        for j in range(len(elements)):
+            sub_sum = sum(c_elements[j: j+i])
             sums.add(sub_sum)
-    
     return len(sums)
 
-# 예제 테스트 케이스
-print(solution([7, 9, 1, 1, 4]))  # 예상 결과: 18
+
+print(solution([7, 9, 1, 1, 4]))
