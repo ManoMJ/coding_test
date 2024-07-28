a460084792ea451e019519decd4120c93d621d4f
ManoMJ <46708194+ManoMJ@users.noreply.github.com>
2024-07-28 23:24:31 +0900

    [level 2] Title: 피보나치 수, Time: 457.65 ms, Memory: 456 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,17 +1,8 @@
 def solution(n):
-    if n <= 1:
-        return n
+    memo = [0] * (n+1)
+    memo[1] = 1
+    
+    for i in range(2, n+1):
+        memo[i] = memo[i-1]+memo[i-2]
+    return memo[n] % 1234567
 
-    MOD = 1234567
-    fibo = [0] * (n + 1)
-    fibo[1] = 1
-
-    for i in range(2, n + 1):
-        fibo[i] = (fibo[i-1] + fibo[i-2]) % MOD
-
-    return fibo[n]
-
-# 테스트
-print(solution(10))  # 55 % 1234567 = 55
-print(solution(20))  # 6765 % 1234567 = 6765
-print(solution(30))  # 832040 % 1234567 = 832040
